929879c2f516420778bde19debb0b0cd9c0d01ae
Fabien <fabien@koality.fr>
2020-03-06 10:39:58 +0100

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,15 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController {
     public function index() {
-        $args = array("content" => "<div style=\"color: red; background: black; padding: 20px;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente reiciendis voluptatibus quis, ea vel architecto culpa fugit libero quaerat soluta quam unde maxime blanditiis voluptatum commodi dolor? Atque, id minus.</div>");
-        return $this->render('index.html.twig', $args);
+        //$args = array("content" => "<div style=\"color: red; background: black; padding: 20px;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente reiciendis voluptatibus quis, ea vel architecto culpa fugit libero quaerat soluta quam unde maxime blanditiis voluptatum commodi dolor? Atque, id minus.</div>");
+        return $this->render('index.html.twig');
     }
-    /*
-    public function index() {
-        $content = "<div style=\"color: red; background: black; padding: 20px;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente reiciendis voluptatibus quis, ea vel architecto culpa fugit libero quaerat soluta quam unde maxime blanditiis voluptatum commodi dolor? Atque, id minus.</div>";
-        return new Response("<html><body><h1>Ready!</h1>$content</body></html>");
-    }
-    */
 
     public function contact(){
         return new Response("<html><body><h1>Contact!</h1></body></html>");
