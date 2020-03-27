2120657999cc23ac892fd0c7baa63bd21f01fa2b
Fabien <fabien@koality.fr>
2020-03-27 11:16:05 +0100

    Typo

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,11 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController {
     public function index() {
-        //$args = array("content" => "<div style=\"color: red; background: black; padding: 20px;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente reiciendis voluptatibus quis, ea vel architecto culpa fugit libero quaerat soluta quam unde maxime blanditiis voluptatum commodi dolor? Atque, id minus.</div>");
-        return $this->render('index.html.twig');
+        $args = array(
+            "content" => "Bienvenue !",
+            'affiche_annonce' => true
+        );
+        return $this->render('content.html.twig', $args);
     }
 
     public function contact(){
-        return new Response("<html><body><h1>Contact!</h1></body></html>");
+        $args = array(
+            "content" => "Contact !",
+            'affiche_annonce' => false
+        );
+        return $this->render('content.html.twig', $args);
     }
 }
